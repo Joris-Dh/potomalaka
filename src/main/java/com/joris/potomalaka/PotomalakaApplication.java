4a4401cee5c15bd3238043f28b3bbf69dea668e8
Joris Dh <joris.dh@outlook.be>
2024-11-20 10:18:40 +0100

    first commit

---
+++ src/main/java/com/joris/potomalaka/PotomalakaApplication.java
@@ -0,0 +1,13 @@
+package com.joris.potomalaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotomalakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotomalakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joris/potomalaka/PotomalakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joris.potomalaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotomalakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
